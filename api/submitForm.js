0c4b0af5a6e07dff77bd3dbfcc4d11372bc8813c
Divyanshu Singh <dsinecos@gmail.com>
2017-12-27 15:09:15 +0530

    Refactor Code
    1. Split emailValidation into another function
    2. Removes redirectOnSubmission from parameters sent as subject of mail
    3. Checks redirectOnSubmission for complete URL with "http://"

--- api/submitForm.js
+++ api/submitForm.js
@@ -1,47 +1,58 @@
 var sendMail = require('../middleware/sendMail.js');
 
+function isEmailValid(email) {
+
+    // Checks that '@' appears only once and '.' appears at least once
+    return (email.indexOf('@') === email.lastIndexOf('@') && email.indexOf('@') !== -1 && email.indexOf('.') !== -1);
+
+}
+
 module.exports = function (req, res, next) {
 
     var mailTo = req.params.email;
 
-    // Checking to ensure there is a single email id (with an @ and . included) in the param
-    if(mailTo.indexOf('@') === mailTo.lastIndexOf('@') && mailTo.indexOf('@') !== -1 && mailTo.indexOf('.') !== -1) {
+    if (isEmailValid(mailTo)) {
         console.log("Email param accepted");
     } else {
-        console.log("Email param not accepted");
+        console.log("Email param invalid");
+        res.status(400).send("Invalid email");
     }
 
     var redirectOnSubmission = "";
+    var mailBody = {};
 
     for (iterate in req.body) {
-        if(iterate === "redirectOnSubmission") {
+        if (iterate === "redirectOnSubmission") {
             redirectOnSubmission = req.body[iterate]
+        } else {
+            mailBody[iterate] = req.body[iterate];
+            console.log(iterate + " : " + req.body[iterate]);
         }
-        console.log("fieldName : " + iterate);
-        console.log("inputData : " + req.body[iterate]);
+
     }
 
-    //var mailTo = req.params.email;
+    var redirectOnSubmission = (redirectOnSubmission.startsWith("http://")) ? redirectOnSubmission : "http://" + redirectOnSubmission;
+
     var mailFrom = 'testingFormHub <me@mydomain.com>';
     var mailSubject = "testing while refactoring code";
-    var mailBody = JSON.stringify(req.body, null, "  ");
+    var mailBody = JSON.stringify(mailBody, null, "  ");
 
-    sendMail(mailTo, mailFrom, mailSubject, mailBody).then(function (data) {
-        console.log(data);
+    sendMail(mailTo, mailFrom, mailSubject, mailBody)
+        .then(function (data) {
+            console.log(data);
 
-        res.redirect(redirectOnSubmission);
-        
-        /*
-        res.status(202).write("This is the body of the request");
-        res.write(JSON.stringify(req.body, null, "  "));
-        res.end();
-        */
+            if (redirectOnSubmission) {
+                res.redirect(redirectOnSubmission);
+            } else {
+                res.send(JSON.stringify(data, null, "  "));
+            }
 
-    }).catch(function (error) {
-        console.log(error);
-        
-        res.status(502).write("Bad Gateway");
-        res.end();
-    });
+        })
+        .catch(function (error) {
+            console.log(error);
+
+            res.status(502).write("Bad Gateway");
+            res.end();
+        });
 
 }
